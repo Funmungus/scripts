005c193fc54a779e9effafabb49b125df2b4636e
Jonathan Pelletier <newparadigmsoftware@gmail.com>
2020-08-05 15:54:24 -0500

    Add ThemedTk

--- setup.py
+++ setup.py
@@ -5,5 +5,4 @@ def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
 if __name__ == '__main__':
-    install('tk')
     install('ttkthemes')
---
+++ win.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+from tkinter import ttk
+from ttkthemes import ThemedTk
+
+window = ThemedTk(theme="arc")
+ttk.Button(window, text="Quit", command=window.destroy).pack()
+window.mainloop()
+
