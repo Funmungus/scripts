88568f5d68a435d8e8c38b909e1aa6ee96ecad2b
Jonathan Pelletier <funmungus@gmail.com>
2021-11-04 12:13:11 -0500

    in-development

--- scripts/auto.py
+++ scripts/auto.py
@@ -1,5 +1,4 @@
 import cv2
-import ctypes
 import numpy as np
 import platform
 import pynput
@@ -8,7 +7,10 @@ from PIL import ImageGrab
 
 mouse = pynput.mouse.Controller()
 
-if platform.system().lower() == "windows":
+WINMODE = platform.system().lower() == "windows"
+if WINMODE:
+    import ctypes
+    import win32gui
     # Query DPI Awareness (Windows 10 and 8)
     awareness = ctypes.c_int()
     errorCode = ctypes.windll.shcore.GetProcessDpiAwareness(0, ctypes.byref(awareness))
@@ -25,32 +27,31 @@ def reduce(image, mult=0.5):
 
 def screen():
     prtscr = ImageGrab.grab()
-    printscreen_numpy = np.array(prtscr, dtype='uint8')\
-    .reshape((prtscr.size[1],prtscr.size[0],3))
-    return printscreen_numpy
+    img_np = np.array(prtscr)
+    return cv2.cvtColor(imp_np, cv2.COLOR_BGR2RGB)
 
 def match_all(image, template, threshold=0.8):
     """ Match all template occurrences which have a higher likelihood than the threshold """
-    height, width = template.shape[:2]
+    h, w = template.shape[:2]
     match_probability = cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED)
     match_locations = np.where(match_probability >= threshold)
 
     # Add the match rectangle to the screen
     rectangles = []
     for x, y in zip(*match_locations[::-1]):
-        rectangles.append([int(x), int(y), int(width), int(height)])
+        rectangles.append([int(x), int(y), int(w), int(h)])
     rectangles, _ = cv2.groupRectangles(rectangles, 1, 0.2)
     return rectangles
 
 def match_best(image, template, threshold=0.8):
-    height, width = template.shape[:2]
+    h, w = template.shape[:2]
     match_probability = cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED)
     # return np.unravel_index(match_probability.argmax(), match_probability.shape)
     # min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(match_probability)
     _, max_val, _, max_point = cv2.minMaxLoc(match_probability)
     if max_val < threshold:
         return None
-    return *max_point, width, height
+    return *max_point, w, h
 
 def screen_find(imagefile, threshold=0.8):
     img = cv2.imread(imagefile)
@@ -67,16 +68,54 @@ def goto_rect(rect):
         mouse.move(int(x + w / 2), int(y + h / 2))
         # mouse.move(int(x + w / 2), int(y + h / 2))
 
-def click_image(imagefile, threshold=0.8, delay=0.1):
-    rect = screen_find(imagefile, threshold)
-    if not rect:
-        return False
-    goto_rect(rect)
-    sleep(0.1)
+def click(delay=0.1):
     mouse.press(pynput.mouse.Button.left)
     sleep(delay)
     mouse.release(pynput.mouse.Button.left)
+
+def click_image(imagefile, threshold=0.8, delay=0.1):
+    rect = screen_find(imagefile, threshold=threshold)
+    if not rect:
+        return False
+    goto_rect(rect)
+    sleep(delay)
+    click(delay=delay)
     return True
+
+def showit(winid):
+    if WINMODE:
+        win32gui.ShowWindow(winid, 5)
+        win32gui.SetForegroundWindow(winid)
+
+def windowEnumerationHandler(hwnd, winlist):
+    winlist.append((hwnd, win32gui.GetWindowText(hwnd)))
+
+def all_windows():
+    if WINMODE:
+        winlist = []
+        win32gui.EnumWindows(windowEnumerationHandler, winlist)
+        return winlist
+    return None
+
+def findwin(name, exact=False):
+    winlist = all_windows()
+    name = name.lower()
+    if exact:
+        for i in winlist:
+            if name == i[1].lower():
+                return i
+    else:
+        for i in winlist:
+            if name in i[1].lower():
+                return i
+    return None
+
+def show_win(name, exact=False):
+    win = findwin(name, exact=exact)
+    if win:
+        showit(win[0])
+        return True
+    return False
 
 def main():
     pass
