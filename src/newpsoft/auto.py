0cdf258a14296b58b0359463db52183a847dd579
Moore Tides <mooretides@gmail.com>
2022-02-26 13:44:21 -0600

    in-development

--- src/newpsoft/auto.py
+++ src/newpsoft/auto.py
@@ -6,7 +6,7 @@ from time import sleep
 from PIL import ImageGrab
 
 mouse = pynput.mouse.Controller()
-keyboard = pynput.mouse.Controller()
+keyboard = pynput.keyboard.Controller()
 
 WINMODE = platform.system().lower() == "windows"
 if WINMODE:
